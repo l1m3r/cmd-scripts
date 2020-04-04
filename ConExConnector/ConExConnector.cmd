4037bac1f1af544e7620920a260fec74d4160a5f
l1m3r <none>
2020-04-04 17:47:05 +0000

    extended/clarified some errMSGs; removed a not yet used function; + some smaller changes

--- ConExConnector/ConExConnector.cmd
+++ ConExConnector/ConExConnector.cmd
@@ -1,5 +1,6 @@
 @echo off
 REM  ---------- Just some preliminary stuff ------------
+set "WSC=   "
 goto :os_chk
 REM OS check
 REM defines arch= -> 1=x86 2=x64 3=W6432  0=err
@@ -42,7 +43,7 @@ REM fc.exe comp.exe sfk.exe
 
 REM Find/check up on required Programs in %requiredPrograms%
 if not defined requiredPrograms goto :prog_find_ret
-	set prog_find_ret=prog_find_ret
+	set "prog_find_ret=prog_find_ret"
 	goto :prog_find
 :prog_find_ret
 if defined prog_missing goto :ERR
@@ -78,7 +79,7 @@ set "cec_regPath=HKCU\Software\Valve\Steam"
 set "cec_regVar=SteamPath"
 set "cec_priorities=LOW BELOWNORMAL NORMAL ABOVENORMAL HIGH REALTIME"
 set "cec_appM=appmanifest_440900.acf"
-(set LF=^
+(set CRLF=^
 %=this line is empty=%
 )
 
@@ -89,7 +90,7 @@ setlocal DISABLEDELAYEDEXPANSION
 set "cec_inparg=%~1" & setlocal ENABLEDELAYEDEXPANSION
 
 set "ERL=20"
-set "errMSG=No config/input file defined. Usage: '%~nx0 <(Full)PathToFile>'"
+set "errMSG=No config/input file defined. Usage: "%~nx0 ^<(Full)PathToFile^>"!CRLF!!CRLF!!WSC!Tip: Create a link to "%~nx0" for every CECcfg-file you use!CRLF!!WSC!     and edit each by attaching the full path to one of those CECcfg-files!CRLF!!WSC!     like in the usage example above."
 if not defined cec_inparg goto :ERR
 REM COULD USE call :chkFiSysObj HERE
 set "errMSG=Couldn't find config/input file '!cec_inparg!'."
@@ -105,9 +106,10 @@ for %%I IN ("!cec_inparg!") do (
 	set "cec_inparg_n=%%~nI"
 	set "cec_inparg_x=%%~xI"
 	set "cec_inparg_a=%%~aI"
+	set "cec_inparg_nx=%%~nxI"
 )
 setlocal ENABLEDELAYEDEXPANSION
-set "errMSG="!cec_inparg_n!!cec_inparg_x!" is not a .!cec_cfgFiExt!-File."
+set "errMSG="!cec_inparg_nx!!cec_inparg_x!" is not a .!cec_cfgFiExt!-File."
 if /I "!cec_inparg_x!" NEQ ".!cec_cfgFiExt!" goto :ERR
 set "errMSG="!cec_inparg!" defines a dir - not a file."
 if /I "!cec_inparg_a:~0,1!" EQU "D" goto :ERR
@@ -121,7 +123,7 @@ for %%I IN (!allUsrVars!) do set "%%I="
 REM Parse the !cec_cfgFiExt!-File and import the defined variables.
 set "ERL=21"
 for /F usebackq^ tokens^=2^ delims^=^" %%I IN (`findstr.exe /B /I /R /C:"set \"%cec_cfgSetStr%..*^=.*\"" "!cec_inparg_f!"`) do set "ERL=0" & set "%%~I"
-set "errMSG="!cec_inparg!" doesn't contain any 'set "!cec_cfgSetStr!^<^>=^<^>"' defintions. (!ERL!)"
+set "errMSG="!cec_inparg!" doesn't contain any 'set "!cec_cfgSetStr!^<^>=^<^>"' defintions."
 if !ERL! NEQ 0 goto :ERR
 
 
@@ -129,20 +131,26 @@ if !ERL! NEQ 0 goto :ERR
 REM   ----------- Find the path to Steam & Conan Exiles. ----------
 set "ERL=22"
 if defined cfg_PathConExSB goto :gotPath
+	set "errMSG=!CRLF!!CRLF!!WSC!You may want to set the correct path manually in your '!cec_inparg_nx!'.!CRLF!!WSC!Look for "cfg_PathConExSB" in there."
 	for /F "usebackq tokens=3 skip=1" %%I IN (`reg.exe query "!cec_regPath!" /v "!cec_regVar!" 2^>NUL `) do set "cfg_PathConExSB=%%~I"
-	set "errMSG=Quering the registry for Steams path failed."
-	if not defined cfg_PathConExSB goto :ERR
-	
-	set "cfg_PathConExSB=!cfg_PathConExSB:/=\!\SteamApps"
-	set "errMSG=Couldn't find "!cec_appM!" in "!cfg_PathConExSB!".!LF!   (for now) This script only works with Conan Exile in Steams default library folder."
-	if not exist "!cfg_PathConExSB!\!cec_appM!" goto :ERR
-	
-	for /F "usebackq tokens=1,*" %%I IN (`findstr.exe /R /I /C:"^		*\"installdir\"		*\"..*\"" "!cfg_PathConExSB!\!cec_appM!"`) do set "ERL=0" & set "cfg_PathConExSB=!cfg_PathConExSB!\common\%%~J\ConanSandbox"
-	set "errMSG=Couldn't find Conans path in "!cec_appM!"."
-	if !ERL! NEQ 0 goto :ERR
-	
+	if not defined cfg_PathConExSB (
+		set "errMSG=Quering the registry for Steams path failed.!errMSG!"
+		goto :ERR
+	)
 	set "ERL=23"
-	set "errMSG=The "ConanSandbox" path doesn't seem to exist."
+	set "cfg_PathConExSB=!cfg_PathConExSB:/=\!\SteamApps"
+	if not exist "!cfg_PathConExSB!\!cec_appM!" (
+		set "errMSG=Couldn't find "!cec_appM!" in "!cfg_PathConExSB!".!CRLF!!WSC!^(for now^) This script only works with Conan Exile in Steams default library folder.!errMSG!"
+		goto :ERR
+	)
+	set "ERL=24"
+	for /F "usebackq tokens=1,*" %%I IN (`findstr.exe /R /I /C:"^		*\"installdir\"		*\"..*\"" "!cfg_PathConExSB!\!cec_appM!" 2^>NUL`) do set "ERL=0" & set "cfg_PathConExSB=!cfg_PathConExSB!\common\%%~J\ConanSandbox"
+	if !ERL! NEQ 0 (
+		set "errMSG=Couldn't find Conans path in "!cec_appM!".!CRLF!!WSC!Have you actually installed the game ^(through Steam^)?!errMSG!."
+		goto :ERR
+	)
+	set "ERL=25"
+	set "errMSG=The "ConanSandbox" path doesn't seem to exist.!errMSG!"
 	if not exist "!cfg_PathConExSB!\." goto :ERR
 :gotPath
 
@@ -156,19 +164,19 @@ set "cec_exe=!cfg_PathConExSB!\Binaries\Win64\ConanSandbox.exe"
 set "errMSG=Conans main executeable doesn't exist where it's supposed to be."
 if not exist "!cec_exe!" goto :ERR
 
-set "ERL=25"
+set "ERL=26"
 if defined cfg_ThisIP goto :gotIP
-set "errMSG=Neither cfg_ThisIP nor cfg_ThisFQDN were defined."
+set "errMSG=Neither cfg_ThisIP nor cfg_ThisFQDN were defined in your '!cec_inparg_nx!'."
 if not defined cfg_ThisFQDN goto :ERR
 
 REM Check/set the process priority.
-set "ERL=26"
+set "ERL=27"
 if not defined cfg_ThisPrio (
 	set "cfg_ThisPrio=ABOVENORMAL"
 	goto :gotPrio
 ) & REM else ...
 	for %%I IN (!cec_priorities!) do if /I "%%~I" EQU "!cfg_ThisPrio!" set "ERL=0"
-	set "errMSG='!cfg_ThisPrio!' is not a priority (!ERL!)."
+	set "errMSG=The valu '!cfg_ThisPrio!' set as priority in '!cec_inparg_nx!' is not allowed."
 	if !ERL! NEQ 0 goto :ERR
 :gotPrio
 
@@ -181,12 +189,13 @@ if not defined cfg_ThisModlist set "cfg_ThisModlist=%~dp0.\cecModList - !cec_inp
 
 
 
-REM   ----------- Lookup the servers IP from its FQDN ----------
+REM   ----------- Lookup the servers IP address from its FQDN ----------
 echo[
 echo[  --- Looking up the IP of "!cfg_ThisFQDN!" (this may take a few seconds).
+set "ERL=28"
 REM -type^=AAAA+A
 for /F "skip=2 tokens=1,2 delims=: " %%I IN ('nslookup.exe -timeout^=!cec_nsluTimeout! -type^=A "!cfg_ThisFQDN!" 2^>NUL') do if /I "%%~I" EQU "!cec_nsluAdrStr!" set "cfg_ThisIP=%%~J"
-set "errMSG=Couldn't find the IP address of '!cfg_ThisFQDN!'."
+set "errMSG=Couldn't find the IP address of '!cfg_ThisFQDN!' (cfg_ThisFQDN)."
 if not defined cfg_ThisIP goto :ERR
 
 
@@ -195,52 +204,46 @@ REM   ----------- Check IP Address ----------
 REM Maybe rewrite with [0-9], [1-9][0-9], 1[0-9][0-9], 2[0-4][0-9], 25[0-5]
 REM FINDSTR /r "^[1-9][0-9]*$ ^0$"
 set "IPpVars=ipp1 ipp2 ipp3 ipp4"
-for %%I IN (!IPpVars!) do set "%%~I="
+for %%I IN (!IPpVars!) do set "%%~I=" & REM Clear vars.
 for /F "tokens=1,2,3,4 delims=." %%I IN ("!cfg_ThisIP!") do (
 	set /A "ipp1=%%~I"
 	set /A "ipp2=%%~J"
 	set /A "ipp3=%%~K"
 	set /A "ipp4=%%~L"
 ) >NUL 2>&1
-set "ERL=0"
+set "ERL=30"
 set "errMSG="
 for %%I IN (!IPpVars!) do if not defined %%~I (
 	set /A ERL+=1
 	set "errMSG=!errMSG!%%~I,"
 )
-if !ERL! NEQ 0 (
-	set "errMSG='!cfg_ThisIP!' is not a legitimate IP. ^(!errMSG!^)"
-	set /A ERL+=10
-	goto :ERR
-)
+set "errMSG='!cfg_ThisIP!' is not a legitimate IPv4 address. ^(!errMSG!^)"
+if !ERL! NEQ 30 goto :ERR
+
 set "blargh=!ipp1!.!ipp2!.!ipp3!.!ipp4!"
-if "!blargh!" NEQ "!cfg_ThisIP!" (
-	set "errMSG='!cfg_ThisIP!' is not a legitimate IP. ^(NEQ !blargh!^)"
-	goto :ERR
-)
-set "erl=4"
-REM
-for %%I IN (!IPpVars!) do if !%%~I! GEQ 0 if !%%~I! LEQ 255 set /A ERL-=1
-set "errMSG='!cfg_ThisIP!' is not a legitimate IP. !ERL! blocks are wrong."
-if !ERL! NEQ 0 (
-	set /A ERL+=50
-	goto :ERR
-)
+set "errMSG='!cfg_ThisIP!' is not a legitimate IPv4 address. ^(NEQ !blargh!^)"
+if "!blargh!" NEQ "!cfg_ThisIP!" goto :ERR
+
+set "erl=35"
+set "cnt=4"
+for %%I IN (!IPpVars!) do if !%%~I! GEQ 0 if !%%~I! LEQ 255 set /A cnt-=1
+set "errMSG='!cfg_ThisIP!' is not a legitimate IP. !cnt! blocks are wrong."
+if !cnt! NEQ 0 goto :ERR
+
 
 
 REM ----------- Check network port ---------------------
 if not defined cfg_ThisPort goto :gotPort
-	set "ERL="
-	set /A "ERL=!cfg_ThisPort!" >NUL 2>&1
-	if "!ERL!" EQU "!cfg_ThisPort!" if !ERL! LEQ 65535 if !ERL! GTR 0 (
-		set "cfg_ThisIP=!cfg_ThisIP!:!cfg_ThisPort!"
-		set "ERL=0"
-		goto :gotPort
-	)
-	set "errMSG=Someting is wrong with your port '!cfg_ThisPort!' (!ERL!).
-	set "ERL=10!ERL!"
+	set "cnt="
+	set /A "cnt=!cfg_ThisPort!" >NUL 2>&1
+	set "cfg_ThisIP=!cfg_ThisIP!:!cfg_ThisPort!"
+	if "!cnt!" EQU "!cfg_ThisPort!" if !cnt! LEQ 65535 if !cnt! GTR 0 goto :gotPort
+	
+	set "ERL=10!cnt!"
+	set "errMSG=Someting is wrong with your port '!cfg_ThisPort!' (too small/big or NEQ !cnt!).
 	goto :ERR
 :gotPort
+
 
 
 REM   ----------- Disable the intro-movies of ConEx ----------
@@ -263,7 +266,7 @@ if defined cfg_KeepIntroClips (
 	)
 	powershell.exe -Command "(Get-Content -Path '!cec_InGameDefCfg!') -ireplace '^\+StartupMovies\=', '-StartupMovies='  -ireplace '^bWaitForMoviesToComplete\=.*$', 'bWaitForMoviesToComplete=False' | Out-File -encoding ASCII '!cec_InGameDefCfg!'"
 	set "ERL=!ERRORLEVEL!"
-	set "errMSG=Failed to modify 'DefaultGame.ini' (!ERL!).
+	set "errMSG=Failed to modify 'DefaultGame.ini'.
 	if %ERL% NEQ 0 goto :ERR
 :KeepIntroMovs
 
@@ -316,13 +319,13 @@ REM if !modlistsAreDiff! EQU 0 goto :noChange
 	echo[   WITHOUT quiting Conan Exiles completly and answereing this question inbetween.
 	echo[
 	set "answer=n"
-	set /P "answer=Answer (yes/no, nothing=no): "
+	set /P "answer=Answer (yes/no, nothing=no, send with enter): "
 	if "!answer!" EQU "!answer:y=!" goto :noChange
 	REM moving the modified/newer internal modlist to cfg_ThisModlist
 	echo[
 	copy /D /V /Y "!cec_InGameModList!" "!cfg_ThisModlist!" >NUL
 	set "ERL=%ERRORLEVEL%"
-	set "errMSG=Overwriting the old with the new list failed (!ERL!).
+	set "errMSG=Overwriting the old with the new list failed.
 	if %ERL% NEQ 0 goto :ERR
 :noChange
 
@@ -337,8 +340,8 @@ REM ------------ Subroutines not belonging to the 'real' script follow here. ---
 echo[
 title %title% - ERROR (%ERL%)
 echo[
-echo[  ERROR:
-echo[   !errMSG!
+echo[  ERROR (%ERL%):
+echo[%WSC%!errMSG!
 echo[
 REM set /A ERL+=1
 :EOFi
@@ -348,7 +351,7 @@ if "%cmdClose%"=="1" (
 	exit
 )
 REM (If cmdClose==0) OR (if not defined cmdClose) -> pause
-title %title% - fin.
+if %ERL% EQU 0 title %title% - fin.
 echo[
 set /A cfg_WaitDelay*=2
 REM echo[  closing in ~!cfg_WaitDelay! seconds ...
@@ -428,86 +431,9 @@ for %%I IN (%requiredPrograms%) do (
 	if !lERL! NEQ 0 (
 		set /A ERL+=1
 		set prog_missing=%%~I
-		set "errMSG=Required programm "%%~I" could not be found (!ERL!)."
+		set "errMSG=Required programm "%%~I" could not be found."
 		set lERL=0
 	)
 )
 set "pf_tmpath="
 goto :%prog_find_ret%
-
-
-
-
-
-:chkFiSysObj
-REM ---------- Begin Check-File-System-Object
-REM Aufruf:
-REM call :chkFiSysObj <obj>, 1full, 2drive, 3path, 4name, 5ext, 6size, 7attr, 8time
-REM Unused Variables between used ones must/can be set to "nul"
-REM return-value = ERL = isFile
-REM   >1	(cnt-1) Object(s) found with placeholders in %~1.
-REM    1	Obj is a file.
-REM    0	Obj is a directory.
-REM   -1	Obj does not exist.
-REM   -2	placeholders in %~1 and no fitting files(!) found. no vars defined.
-REM   -3	forbidden chars in %~1.
-REM if not defined (6, 7 or 8) -> object does not exist.
-%RemOrVerbRun% echo[
-%RemOrVerbRun% echo[ ---- %~0 begin - %*
-set "chkFSO_Obj=%~1"
-REM Cleanup input
-set "chkFSO_Obj=!chkFSO_Obj:&=!"
-set "chkFSO_Obj=!chkFSO_Obj:|=!"
-set "chkFSO_Obj=!chkFSO_Obj:>=!"
-set "chkFSO_Obj=!chkFSO_Obj:<=!"
-set "chkFSO_Obj=!chkFSO_Obj:"=!"
-if "!chkFSO_Obj!" NEQ "%~1" (
-	set ERL=-3
-	REM verbotene Zeichen in %~1
-	%RemOrVerbRun% echo[ ---- %~0 - input contains forbidden chars.
-	goto :EOF_chkFiSysObj
-)
-shift /1
-set "chkFSO_atr="
-set ERL=0
-for %%I IN ("!chkFSO_Obj!") do (
-	set "chkFSO_atr=%%~aI"
-	set /A ERL+=1
-	set "%~1=%%~fI"
-	set "%~2=%%~dI"
-	set "%~3=%%~pI"
-	set "%~4=%%~nI"
-	set "%~5=%%~xI"
-	set "%~6=%%~zI"
-	set "%~7=%%~aI"
-	set "%~8=%%~tI"
-) 2>nul
-REM set ERL= return-value / isFile
-if %ERL% EQU 0 (
-	REM no files found.
-	%RemOrVerbRun% echo[ ---- %~0 - no file^(s^) found, no vars defined.
-	set ERL=-2
-	goto :EOF_chkFiSysObj
-)
-echo["!chkFSO_Obj!" | findstr.exe "* ?" 2>&1 >nul
-if !ERRORLEVEL! EQU 0 (
-	REM input contains placeholders -> erl++
-	%RemOrVerbRun% echo[ ---- %~0 - %ERL% object^(s^) found ^(with placeholders^).
-	set /A ERL+=1
-) else if %ERL% EQU 1 if not defined chkFSO_atr (
-	REM obj is nonexistent.
-	%RemOrVerbRun% echo[ ---- %~0 - obj is nonexistent, some vars defined.
-	set ERL=-1
-) else if /I "D"=="%chkFSO_atr:~0,1%" (
-	REM obj is a directory.
-	%RemOrVerbRun% echo[ ---- %~0 - obj is a directory, vars defined.
-	set ERL=0
-)
-:EOF_chkFiSysObj
-REM Cleanup
-for /F "usebackq delims==" %%I IN (`set chkFSO_`) do set "%%I="
-if defined nul set "nul="
-%RemOrVerbRun% echo[ ---- %~0 end.
-exit /B %ERL%
-REM ---------- End Check-File-System-Object
-
